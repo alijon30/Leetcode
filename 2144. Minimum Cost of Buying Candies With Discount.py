bbf5a52c4f5274fb8cff9ed22c9f2f4fbcface16
Alijon Karimberdiev <79594266+alijon30@users.noreply.github.com>
2022-02-10 13:26:01 -0500

    Create 2144. Minimum Cost of Buying Candies With Discount.py

---
+++ 2144. Minimum Cost of Buying Candies With Discount.py
@@ -0,0 +1,35 @@
+A shop is selling candies at a discount. For every two candies sold, the shop gives a third candy for free.
+
+The customer can choose any candy to take away for free as long as the cost of the chosen candy is less than or equal to the minimum cost of the two candies bought.
+
+For example, if there are 4 candies with costs 1, 2, 3, and 4, and the customer buys candies with costs 2 and 3, they can take the candy with cost 1 for free, but not the candy with cost 4.
+Given a 0-indexed integer array cost, where cost[i] denotes the cost of the ith candy, return the minimum cost of buying all the candies.
+
+ 
+
+Example 1:
+
+Input: cost = [1,2,3]
+Output: 5
+Explanation: We buy the candies with costs 2 and 3, and take the candy with cost 1 for free.
+The total cost of buying all candies is 2 + 3 = 5. This is the only way we can buy the candies.
+Note that we cannot buy candies with costs 1 and 3, and then take the candy with cost 2 for free.
+The cost of the free candy has to be less than or equal to the minimum cost of the purchased candies.
+
+
+
+
+
+
+
+class Solution:
+    def minimumCost(self, cost):
+        cost.sort(reverse=True)
+        counted = 0
+        sum = 0
+        for i in range(len(cost)):
+            sum += cost[i]
+            counted += 1
+            if counted % 3 == 0:
+                sum -= cost[i]
+        return sum
