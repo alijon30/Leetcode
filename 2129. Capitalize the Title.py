c683c1122a83db8c1bd1c7fe7955c211758376fa
Alijon Karimberdiev <79594266+alijon30@users.noreply.github.com>
2022-02-10 18:26:07 -0500

    Create 2129. Capitalize the Title.py

---
+++ 2129. Capitalize the Title.py
@@ -0,0 +1,47 @@
+You are given a string title consisting of one or more words separated by a single space, where each word consists of English letters. Capitalize the string by changing the capitalization of each word such that:
+
+If the length of the word is 1 or 2 letters, change all letters to lowercase.
+Otherwise, change the first letter to uppercase and the remaining letters to lowercase.
+Return the capitalized title.
+
+ 
+
+Example 1:
+
+Input: title = "capiTalIze tHe titLe"
+Output: "Capitalize The Title"
+Explanation:
+Since all the words have a length of at least 3, the first letter of each word is uppercase, and the remaining letters are lowercase.
+Example 2:
+
+Input: title = "First leTTeR of EACH Word"
+Output: "First Letter of Each Word"
+Explanation:
+The word "of" has length 2, so it is all lowercase.
+The remaining words have a length of at least 3, so the first letter of each remaining word is uppercase, and the remaining letters are lowercase.
+
+
+
+
+
+
+
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        strin = title.lower().split()
+        result = ""
+        for i in range(len(strin)):
+            if len(strin[i]) <= 2:
+                if result == "":
+                    result += strin[i]
+                else:
+                    result += " " + strin[i]
+            else:
+                if result == "":
+                    result += strin[i].capitalize()
+                else:
+                    result += " " +  strin[i].capitalize()
+        return result
+      
+      
+      
