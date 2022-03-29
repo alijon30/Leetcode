2b92c362bec787076b05fcb55aa5fcc51b62b4aa
Alijon Karimberdiev <79594266+alijon30@users.noreply.github.com>
2022-03-29 19:04:27 -0400

    Update 1047. Remove All Adjacent Duplicates In String.py

--- 1047. Remove All Adjacent Duplicates In String.py
+++ 1047. Remove All Adjacent Duplicates In String.py
@@ -61,6 +61,77 @@ class Stack:
         else:
             return self.list[len(self.list)-1]
 
+         
+         
+         
+         
+         
+         ---------------------------------------
+         
+         
+         class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = Stack()
+        
+        for i in range(len(s)):
+            if stack.peek() == s[i]:
+                stack.pop()
+                continue
+            stack.push(s[i])
+        
+        return str(stack)[::-1]
+        
+        
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+            
+class Stack:
+    def __init__(self):
+        self.Linked = LinkedList()
+    
+    def __str__(self):
+        values = [str(val.value) for val in self.Linked]
+        return ''.join(values)
+
+        
+    def isEmpty(self):
+        if self.Linked.head == None:
+            return True
+        else:
+            return False
+    
+    def push(self, value):
+        newNode = Node(value)
+        newNode.next = self.Linked.head
+        self.Linked.head = newNode
+    
+    def pop(self):
+        if self.isEmpty():
+            return "The stack is empty"
+        else:
+            temp = self.Linked.head
+            self.Linked.head = self.Linked.head.next
+            return temp
+    def peek(self):
+        if self.isEmpty():
+            return "The Stack is empty"
+        else:
+            node = self.Linked.head.value
+            return node
+        
+        
 
           
           
