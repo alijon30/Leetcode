827d5bd91eff82c85bc04ecbeb5f271c6ece46fc
Alijon Karimberdiev <79594266+alijon30@users.noreply.github.com>
2022-09-08 09:59:18 -0400

    Create 1721. Swapping Nodes in a Linked List.py

---
+++ 1721. Swapping Nodes in a Linked List.py
@@ -0,0 +1,40 @@
+You are given the head of a linked list, and an integer k.
+
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [1,4,3,2,5]
+Example 2:
+
+Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+Output: [7,9,6,6,8,7,3,0,9,5]
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        dummy = tail = (ListNode)
+        lst = []
+        while head:
+            lst.append(head.val)
+            head = head.next
+        
+        lst[k-1], lst[-k] = lst[-k], lst[k-1]
+        
+        for i in lst:
+            tail.next = ListNode(i)
+            tail = tail.next
+            
+        return dummy.next
+      
+      
+      
